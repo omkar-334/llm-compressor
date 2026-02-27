de455bde01f970aae457f8625add304d8989ef8c
Omkar Kabde <omkarkabde@gmail.com>
2026-02-27 12:36:43 +0530

    add deprecation warning to current filepath

--- src/llmcompressor/modifiers/quantization/gptq/__init__.py
+++ src/llmcompressor/modifiers/quantization/gptq/__init__.py
@@ -1,3 +1,22 @@
 # ruff: noqa
+"""
+Backwards compatibility shim for GPTQModifier.
 
-from .base import *
+This module has been moved to llmcompressor.modifiers.gptq.
+This shim will be removed in a future version.
+"""
+
+import warnings
+
+warnings.warn(
+    "Importing from 'llmcompressor.modifiers.quantization.gptq' is deprecated. "
+    "Please update your imports to use 'llmcompressor.modifiers.gptq' instead. "
+    "This compatibility shim will be removed in a future version.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+from llmcompressor.modifiers.gptq import *
+from llmcompressor.modifiers.gptq.base import GPTQModifier
+
+__all__ = ["GPTQModifier"]
